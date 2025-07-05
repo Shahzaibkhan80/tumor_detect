38da2dd5d7dda4d3b11975eb87129c3f3e617fcd
Shahzaibkhan80 <kamshahzaib@gmail.com>
2025-07-05 23:30:47 +0500

    model 2

--- predictmodel.py
+++ predictmodel.py
@@ -8,14 +8,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Load models safely using relative paths
 stage_model_path = os.path.join(BASE_DIR, 'models', 'modelsarcoma.h5')
-# modelpredict_path = os.path.join(BASE_DIR, 'models', 'newmodel.h5')
+modelpredict_path = os.path.join(BASE_DIR, 'models', 'newmodel.h5')
 # brain_model_path = os.path.join(BASE_DIR, 'models', 'braintumor.h5')
 
 stage_model = tf.keras.models.load_model(stage_model_path)
 print("Stage Model loaded successfully")
 
-# modelpredict = tf.keras.models.load_model(modelpredict_path)
-# print("Predict Model loaded successfully")
+modelpredict = tf.keras.models.load_model(modelpredict_path)
+print("Predict Model loaded successfully")
 
 # brain_model = tf.keras.models.load_model(brain_model_path)
 # print("Brain MRI Model loaded successfully.")
